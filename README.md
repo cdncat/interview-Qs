519d368869a6a4bbc5911f9c85f517df95514445
cdncat <cdncat@users.noreply.github.com>
2018-02-17 03:02:02 -0500

    [coins] cleaned up main.py for coins Q

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Interview code Q's
 ==================
+A place to keep a list of some questions I encountered in interviews while looking for internships.
 
-* coins Q
+* coinbag & amount Q (added: Feb 17/18)
+  * current implemention is recursive, could be optimized further to be pseudo-linear
+  * current runtime: O(n^2)
+
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+## Coinbag Question
+
+# cc stands for coinCombos
+# For a list of coins, where each element represents a coin's value
+# and (monetary) amount:
+# calculate the number of ways we combine the elements in coins to sum to amount
+# can use each element 0, 1, or more times
+# example: cc(5, [1,2]) returns 3
+
 def cc(amount, coins):
-  print(amount, coins)
   counter = 0
+
   if not coins:
     return 0
-    
+
   for i, coin in enumerate(coins):
     if coin == amount:
       counter+=1
@@ -12,18 +21,17 @@ def cc(amount, coins):
       continue
     if amount%coin == 0:
       counter+=1
-      
+
     quotient = amount//coin
-    
+
     for j in range(1,quotient+1):
       result = cc(amount-j*coin, coins[i+1:])
       counter+=result
 
   return counter
-      
-    
+
+# some test cases:
 # print(cc(1, [1]))
 # print(cc(2, [1,2]))
-# print(cc(1, [2]))
-# print(cc(2, [1]))
-print(cc(5, [1,2,3]))
+# print(cc(5, [1,2,3]))
+# print(cc(7, [4]))
